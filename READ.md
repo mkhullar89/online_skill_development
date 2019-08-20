c4d517d4fab288651de0b1920857b0f2e76e2145
Manav Khullar <mkhullar89@gmail.com>
2019-08-20 12:24:10 +0530

    Initial commit

---
+++ READ.md
@@ -0,0 +1,2 @@
+Hello This is A Test To Check IT Out How I Can I do Project 
+On Django
--- online_skill_assesment/settings.py
+++ online_skill_assesment/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'userman.apps.UsermanConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
